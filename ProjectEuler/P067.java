1ea31b0d8a89687d0c277c38d38c824958db9689
phonism <sdu.phonism@gmai.com>
2014-06-15 15:32:30 +0800

    add pe 18, 67

---
+++ ProjectEuler/P067.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class P067 {
+
+	Scanner scan = new Scanner(System.in);
+
+	public void run() {
+		int n = scan.nextInt();
+		int[][] a = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				a[i][j] = scan.nextInt();
+			}
+		}
+		int[][] f = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			f[n][i] = a[n][i];
+		}
+		for (int i = n - 1; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				f[i][j] = Math.max(f[i + 1][j], f[i + 1][j + 1]) + a[i][j];
+			}
+		}
+		System.out.println(f[1][1]);
+	}
+
+	public static void main(String args[]) {
+		new P067().run();
+	}
+}
